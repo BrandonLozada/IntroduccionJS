1b41227468dcd9a6d95fa13a67c1e59f8b59324b
Brandon Lozada <brandon_lozada@outlook.com>
2023-06-01 15:24:33 -0600

    Subir ejercicio 1

---
+++ README.md
@@ -0,0 +1,4 @@
+# IntroduccionJS
+
+Estos son ejercicios son realizados siguiendo el curso de 
+"Vue.js 3 - La Guía Completa - Composition Pinia MEVN 10 Apps" en Udemy.com
---
+++ js/01.js
@@ -0,0 +1,16 @@
+// Variables con let
+let cliente = 'Brandon'
+
+// Se pueden re-asignar
+cliente = 'Brandon Lozada'
+cliente = 69
+cliente = true
+
+// Pueden iniciar sin un valor
+let precio
+
+precio = 369
+
+console.log('cliente: ', cliente)
+console.log('precio: ', precio)
+
