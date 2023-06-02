64258f4a96b0f6f5774a5b12019506974af7ba7e
Brandon Lozada <brandon_lozada@outlook.com>
2023-06-02 11:00:31 -0600

    Subir ejercicio 4

---
+++ js/04.js
@@ -0,0 +1,33 @@
+// Objetos
+
+// const nombreProducto = "Tablet"
+// const precioProducto = 300
+// const disponible = true
+
+// const producto = {
+//     nombre : "Tablet",
+//     precio : 300,
+//     disponible : true
+// }
+
+// console.log(typeof producto)
+// console.table(producto)
+// console.log(producto.nombre)
+// console.log(producto.precio)
+
+// Destructuring
+// const { nombre, disponible } = producto
+
+// console.log(nombre)
+// console.log(disponible)
+
+// Object literal | Object literal enhancement
+const autenticado = true
+const usuario = 'Brandon'
+
+const nuevoObjeto = {
+    autenticado : autenticado,
+    usuario : usuario
+}
+
+console.log(nuevoObjeto)
