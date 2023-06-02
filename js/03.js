e212750782b03f1c488d13c8bbb7ea612fe71f96
Brandon Lozada <brandon_lozada@outlook.com>
2023-06-02 10:47:57 -0600

    Subir ejercicio 3

---
+++ js/03.js
@@ -0,0 +1,59 @@
+// Tipos de datos
+
+// Undefined
+// let cliente
+// console.log(typeof cliente)
+
+// Boolean
+// const descuento = true
+// console.log(typeof descuento)
+
+// Number
+// const numero1 = 20.30
+// const numero2 = 69
+// const numero3 = -200
+
+// console.log(typeof numero1)
+// console.log(typeof numero2)
+// console.log(typeof numero3)
+
+// Strings o cadenas de texto
+// const alumno = 'Brandon'
+// let producto = 'Monitor 69 pulgadas'
+
+// console.log(typeof alumno)
+// console.log(typeof producto)
+
+// const numero = 69
+// const numero2 = 96
+
+// console.log(typeof numero)
+// console.log(typeof numero2)
+
+// BigInt
+// const numeroGrande = BigInt(1)
+// const numero = 69
+// const numero2 = 96
+
+// console.log(typeof numeroGrande)
+// console.log( numero + Number(numeroGrande) )
+
+// Symbols
+// const primerSymbol = Symbol(1)
+// const segundoSymbol = Symbol(1)
+
+// const numero = 10
+// const numero2 = 10
+
+// console.log(primerSymbol, segundoSymbol)
+// console.log(primerSymbol === segundoSymbol)
+// console.log(numero === numero2)
+
+// Object
+const numero = [1,2,3]
+const descuento = null
+const alumno = {}
+console.log(typeof numero)
+console.log(typeof descuento)
+console.log(typeof alumno)
+
