9a4331be53392df1f35a218b9b10e77cc0dbe9ec
Brandon Lozada <brandon_lozada@outlook.com>
2023-06-05 12:33:07 -0600

    Subir ejercicio 13

---
+++ js/13.js
@@ -0,0 +1,14 @@
+// Function Declaration
+
+function sumar (numero1 = 0, numero2 = 10) {
+    console.log(numero1 + numero2)
+
+    // console.log(typeof numero1)
+    // console.log(typeof numero2)
+}
+
+sumar(65,4)
+sumar(54,5)
+sumar()
+
+sumar(1, 2, 3, 4, 6, 7, 98)
