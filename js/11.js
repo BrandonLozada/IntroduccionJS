b24fe775eb9ac2eb08c6a41d7357e9204e9cf3c7
Brandon Lozada <brandon_lozada@outlook.com>
2023-06-05 11:47:19 -0600

    Subir ejercicio 11

---
+++ js/11.js
@@ -0,0 +1,17 @@
+// Destructuring de arrays
+const tecnologias = ['Vue.js', 'Javascript', 'Node.js', 'HTML', 'CSS']
+
+// Destrucuración - versión corta
+// const [ vuejs, js, node ] = tecnologias
+const [ vuejs, js, , html ] = tecnologias
+
+// Destrucuración - versión larga
+// const vuejs = tecnologias[0]
+// const js = tecnologias[1]
+// const html = tecnologias[3]
+
+
+console.log(vuejs)
+console.log(js)
+// console.log(node)
+console.log(html)
