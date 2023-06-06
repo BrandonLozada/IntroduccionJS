f0335a9a038828850fb43ab5f6521e55922ef60c
Brandon Lozada <brandon_lozada@outlook.com>
2023-06-06 14:13:19 -0600

    Subir ejercicio 29

---
+++ js/29.js
@@ -0,0 +1,8 @@
+// ESM - EcmaScript Modules
+import texto, { sumar as suma, restar } from './funciones.js'
+
+console.log(suma(10, 20))
+console.log(restar(100, 80))
+
+console.log(texto)
+
---
+++ js/funciones.js
@@ -0,0 +1,25 @@
+// Exportar Function
+// export function sumar(numero1, numero2) {
+//     return numero1 + numero2
+// }
+
+// export function restar(numero1, numero2) {
+//     return numero1 - numero2
+// }
+
+////////////////////////////////////////////
+function sumar(numero1, numero2) {
+    return numero1 + numero2
+}
+
+function restar(numero1, numero2) {
+    return numero1 - numero2
+}
+
+const hola = 'Hola Mundo' 
+export default hola
+
+export {
+    sumar, 
+    restar
+}
