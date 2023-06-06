4c2409f32f52918860393a1204594c29d2162e48
Brandon Lozada <brandon_lozada@outlook.com>
2023-06-06 08:46:12 -0600

    Subir ejercicio 18

---
+++ js/18.js
@@ -0,0 +1,14 @@
+// Operador de comparación
+const numero1 = 20
+const numero2= '20'
+
+// === Comparación de valor y tipo de dato
+// ==  Comparación de valor
+if (numero1 === numero2) {
+    console.log('Sí son iguales')
+} else {
+    console.log('No, no son iguales')
+}
+
+console.log(typeof numero1)
+console.log(typeof numero2)
