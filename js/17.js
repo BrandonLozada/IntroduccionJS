de4578c8ac287c29e61b5cb38d648bec7c4ed935
Brandon Lozada <brandon_lozada@outlook.com>
2023-06-06 08:41:16 -0600

    Subir ejercicio 17

---
+++ js/17.js
@@ -0,0 +1,15 @@
+// Condicionales
+const disponible = 100
+const retirar = 69
+
+// if (disponible > retirar) {
+//     console.log('Puedes retirar')
+// } else {
+//     console.log('No puedes retirar esa cantidad')
+// }
+
+// If Ternario
+const resultado = disponible > retirar ? 'Puedes retirar' : 'No puedes returar esa cantidad'
+
+console.log(resultado)
+
